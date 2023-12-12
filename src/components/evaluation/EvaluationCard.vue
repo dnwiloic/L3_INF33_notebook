af97ece5c1d1721e8a890c7d928226852ae11ba5
Dnjomou Loic <dnjomouloic@gmail.com>
2023-12-12 13:47:46 +0100

    evaluation in doing

---
+++ src/components/evaluation/EvaluationCard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+const props = defineProps(['question'])
+
+const response = ref('')
+</script>
+<template>
+    <div id="question-card">
+        <div class="question-card-form">
+            <div class="mb-3">
+                <label for="note-title" class="col-form-label">numbre de notes :</label>
+                <input type="number" class="form-control" id="note-title" v-model="response">
+            </div>
+            <div class="d-flex justify-content-end">
+                <button class="btn btn-primary ms-auto">
+                    Suivant
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#question-card{
+    height: 100vh;
+    justify-content: center;
+    align-content: center;
+}
+
+.question-card-form{
+    background-color: white;
+    padding: 2em;
+    margin-top: 3em;
+    max-width: 25em;
+    margin-inline: auto;
+}
+</style>
---
+++ src/views/evaluation/PassEvaluation.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import EvaluationCard from '@/components/evaluation/EvaluationCard.vue';
+import {ref } from 'vue'
+
+</script>
+<template>
+    <div>
+        <EvaluationCard />
+        <div class="m-3" >
+            <div class="d-flex justify-content-end align-item-center">
+                <button class="btn changePage" @click="currentPage--" :disabled="currentPage===1">Prev</button>
+                <button class="btn changePage" v-for="i in showerIndexPage" @click="currentPage=i" :class="currentPage===i?'current':''" :key="''+i">
+                    {{ i }} 
+                </button>
+                <button class="btn changePage" @click="currentPage++" :disabled="currentPage===nbrPages">Next</button>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+
+</style>
